770b7b8789163850a7f89dd8a578e877e16e726d
Jonathan Byte <jonathan.cruzpalacios@outlook.com>
2022-04-15 23:40:42 -0600

    Test to Post and User failed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :published, inclusion: { in: [true, false] }
+  validates :user_id, presence: true
 end
---
+++ test/factories/posts.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :post do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { !rand(0..1).zero? }
+    user
+  end
+end
